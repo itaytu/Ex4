4463386b99be19c30e49832f9abbf8aa628eeb03
itaytu <itaytu93@gmail.com>
2019-01-13 14:29:19 +0200

    Final update

--- JavaDoc/package-search-index.js
+++ JavaDoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"Algorithms"},{"l":"All Packages","url":"allpackages-index.html"},{"l":"controller"},{"l":"Converters"},{"l":"Coords"},{"l":"Elements"},{"l":"Geom"},{"l":"Graph"},{"l":"GUI"},{"l":"Tests"},{"l":"Utils"}]
+packageSearchIndex = [{"l":"Algorithms"},{"l":"All Packages","url":"allpackages-index.html"},{"l":"controller"},{"l":"Converters"},{"l":"Coords"},{"l":"Elements"},{"l":"Geom"},{"l":"Graph"},{"l":"GUI"},{"l":"observer"},{"l":"Tests"},{"l":"Utils"}]
